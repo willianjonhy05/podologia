281eb317ae047619410f1d0e95439896ed47d33c
willianjohny05 <willianjohny05@gmail.com>
2024-11-19 20:11:24 -0300

    Criação de novas classes

--- core/models.py
+++ core/models.py
@@ -1,5 +1,255 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from datetime import date
+from django.core.exceptions import ValidationError
+
+
+class Usuario(models.Model):
+    nome = models.CharField('Nome', max_length=255)
+    data_nasc = models.DateField('Data de Nascimento', null=True, blank=True)
+    foto = models.ImageField("Foto", upload_to='avatares', blank=True, null=True)
+    email = models.EmailField('E-mail')
+    telefone = models.CharField("Telefone", max_length=15, null=True, blank=True)
+    cpf = models.CharField("CPF", max_length=15, null=True, blank=True)
+    user = models.OneToOneField(
+        get_user_model(),
+        verbose_name="Usuário",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="usuario"
+    )
+
+    @property
+    def idade(self):
+        if self.data_nasc:
+            hoje = date.today()
+            diferenca = hoje - self.data_nasc
+            return round(diferenca.days // 365.25)
+        return None
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+
+
+class Disponibilidade(models.Model):
+    DIAS_DA_SEMANA = [
+        ('segunda', 'Segunda-feira'),
+        ('terca', 'Terça-feira'),
+        ('quarta', 'Quarta-feira'),
+        ('quinta', 'Quinta-feira'),
+        ('sexta', 'Sexta-feira'),
+        ('sabado', 'Sábado'),
+        ('domingo', 'Domingo'),
+    ]
+
+    dia = models.CharField(
+        max_length=10,
+        choices=DIAS_DA_SEMANA,
+        verbose_name="Dia da Semana"
+    )
+    horario_inicio = models.TimeField(verbose_name="Horário de Início")
+    horario_fim = models.TimeField(verbose_name="Horário de Fim")
+
+    class Meta:
+        verbose_name = "Disponibilidade"
+        verbose_name_plural = "Disponibilidades"
+        ordering = ['dia', 'horario_inicio']
+
+    def __str__(self):
+        return f"{self.get_dia_display()} ({self.horario_inicio} às {self.horario_fim})"
+
+    def clean(self):
+        if self.horario_inicio >= self.horario_fim:
+            raise ValidationError("O horário de fim deve ser posterior ao horário de início.")
+
+
+class ProfissionalDePodologia(models.Model):
+    nome = models.CharField("Nome", max_length=100)
+    especializacao = models.CharField(
+        max_length=300,
+        help_text="Especialização, como atendimento infantil, TEA/TDAH etc."
+    )
+    user = models.OneToOneField(
+        get_user_model(),
+        verbose_name="Usuário",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="profissional"
+    )
+    foto = models.ImageField("Foto", upload_to='avatares', blank=True, null=True)
+    email = models.EmailField("E-mail", help_text="E-mail de contato do profissional")
+    telefone_whatsapp = models.CharField(
+        "Telefone/WhatsApp",
+        max_length=15,
+        null=True,
+        blank=True,
+        help_text="Telefone de contato"
+    )
+    rede_social = models.URLField(
+        "Link de Rede Social",
+        null=True,
+        blank=True,
+        help_text="Website ou página de contato do profissional"
+    )
+    disponibilidade = models.ManyToManyField(
+        Disponibilidade,
+        verbose_name="Disponibilidades",
+        related_name="profissionais"
+    )
+    endereco = models.CharField(
+        "Endereço",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Endereço"
+    )
+    bairro = models.CharField(
+        "Bairro",
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text="Bairro"
+    )
+    especialidade = models.TextField(
+        "Especialidade",
+        help_text="Descrição da especialidade do profissional e sua experiência"
+    )
+    aprovado = models.BooleanField("Aprovado", default=False)
+
+    class Meta:
+        verbose_name = "Profissional de Podologia"
+        verbose_name_plural = "Profissionais de Podologia"
+
+    def __str__(self):
+        return self.nome
+
+
+class TratamentoPodologico(models.Model):
+    TIPOS_TRATAMENTO = [
+        ('Preventivo', 'Preventivo'),
+        ('Estético', 'Estético'),
+        ('Clínico', 'Clínico'),
+        ('Reabilitação', 'Reabilitação'),
+    ]
+
+    nome = models.CharField(max_length=100, verbose_name="Nome do Tratamento")
+    descricao = models.TextField(verbose_name="Descrição", help_text="Breve descrição do tratamento.")
+    duracao = models.PositiveIntegerField(verbose_name="Duração (minutos)", help_text="Duração média do tratamento.")
+    preco = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço")
+    tipo = models.CharField(max_length=20, choices=TIPOS_TRATAMENTO, verbose_name="Tipo do Tratamento")
+
+    class Meta:
+        verbose_name = "Tratamento Podológico"
+        verbose_name_plural = "Tratamentos Podológicos"
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
+
+
+class Agendamento(models.Model):
+    STATUS_CHOICES = [
+        ('pendente', 'Pendente'),
+        ('confirmado', 'Confirmado'),
+        ('concluido', 'Concluído'),
+        ('cancelado', 'Cancelado'),
+    ]
+
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, verbose_name="Cliente", related_name="agendamentos")
+    profissional = models.ForeignKey(
+        ProfissionalDePodologia, on_delete=models.CASCADE, verbose_name="Profissional", related_name="agendamentos"
+    )
+    servicos = models.ManyToManyField(TratamentoPodologico, verbose_name="Serviços", related_name="agendamentos")
+    data = models.DateField(verbose_name="Data do Agendamento", default=date.today)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='pendente',
+        verbose_name="Status do Agendamento"
+    )
+
+    class Meta:
+        verbose_name = "Agendamento"
+        verbose_name_plural = "Agendamentos"
+        ordering = ['data']
+
+    def __str__(self):
+        return f"Agendamento em {self.data} - {self.usuario.nome}"
+
+    def marcar_concluido(self):
+        self.status = 'concluido'
+        self.save()
+
+    def cancelar(self):
+        self.status = 'cancelado'
+        self.save()
+
+
+
+class Feedback(models.Model):
+    NOTA_CHOICES = [
+        (1, '1 - Muito ruim'),
+        (2, '2 - Ruim'),
+        (3, '3 - Regular'),
+        (4, '4 - Bom'),
+        (5, '5 - Excelente'),
+    ]
+
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        verbose_name="Cliente",
+        related_name="feedbacks"
+    )
+    agendamento = models.OneToOneField(
+        Agendamento,
+        on_delete=models.CASCADE,
+        verbose_name="Agendamento",
+        related_name="feedback"
+    )
+    nota = models.PositiveSmallIntegerField(
+        choices=NOTA_CHOICES,
+        verbose_name="Nota",
+        help_text="Nota de 1 a 5 sobre o atendimento"
+    )
+    comentario = models.TextField(
+        verbose_name="Comentário",
+        blank=True,
+        null=True,
+        help_text="Comentários adicionais sobre o atendimento"
+    )
+    data = models.DateTimeField(
+        verbose_name="Data do Feedback",
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = "Feedback"
+        verbose_name_plural = "Feedbacks"
+        ordering = ['-data']
+
+    def __str__(self):
+        return f"Feedback de {self.usuario.nome} - Nota: {self.nota}"
+
+    def clean(self):
+        """
+        Validações adicionais:
+        - Garante que o agendamento já foi concluído antes de receber feedback.
+        """
+        if self.agendamento.status != 'concluido':
+            raise ValidationError("O feedback só pode ser enviado para atendimentos concluídos.")
+
+
+
+
+
+############################################################################################################
 
 class Usuario(AbstractUser):
     """
@@ -28,6 +278,29 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Usuario(models.Model):
+    nome = models.CharField('Nome', max_length=255)
+    data_nasc = models.DateField('Data de Nascimento', null=True, blank=True)
+    foto = models.ImageField("Foto", upload_to='avatares', blank=True, null=True)
+    email = models.EmailField('E-mail')
+    telefone = models.CharField("Telefone", max_length=15, null=True, blank=True)
+    cpf = models.CharField("CPF", max_length=15, null=True, blank=True)
+    user = models.OneToOneField(get_user_model(), verbose_name="Usuário", on_delete=models.CASCADE, null=True, blank=True, related_name="usuario")
+
+    @property
+    def idade(self):
+        hoje = date.today()
+        diferenca = hoje - self.data_nasc
+        return round(diferenca.days // 365.25)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
 
 class RelatorioProgresso(models.Model):
     """
--- podologia/settings.py
+++ podologia/settings.py
@@ -73,14 +73,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'banco8db',
+#         'USER': 'user8',
+#         'PASSWORD': 'Samuca10x',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'banco8db',
-        'USER': 'user8',
-        'PASSWORD': 'Samuca10x',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
 
